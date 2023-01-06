d1ee7d6a81abf6e0d0b9844a8ec6b2347e57c2d0
ContactJHC <95849631+ContactJHC@users.noreply.github.com>
2023-01-06 12:01:30 +0100

    database  for Transactions.jsx

---
+++ src/data/transactionsData.js
@@ -0,0 +1,61 @@
+const transactionsDataHeader = [
+    {
+        accountId: "1",
+        title: "Argent Bank Checking (x8349)",
+        balance: "$2,082.79",
+        legend: "Available Balance"
+    },
+    {
+        accountId: "2",
+        title: "Argent Bank Savings (x6712)",
+        balance: "$10,928.42",
+        legend: "Available Balance"
+    },
+    {
+        accountId: "3",
+        title: "Argent Bank Credit Card (x8349)",
+        balance: "$184.30",
+        legend: "Current Balance"
+    }
+]
+
+const transactionsData = [
+    {
+        date: "June 20th, 2020",
+        description: "Golden Sun Bakery",
+        amount: "$5.00",
+        balance: "$2082.79"
+    },
+    {
+        date: "June 20th, 2020",
+        description: "Golden Sun Bakery",
+        amount: "$10.00",
+        balance: "$2087.79"
+    },
+    {
+        date: "June 20th, 2020",
+        description: "Golden Sun Bakery",
+        amount: "$20.00",
+        balance: "$2097.79"
+    },
+    {
+        date: "June 20th, 2020",
+        description: "Golden Sun Bakery",
+        amount: "$30.00",
+        balance: "$2117.79"
+    },
+    {
+        date: "June 20th, 2020",
+        description: "Golden Sun Bakery",
+        amount: "$40.00",
+        balance: "$2147.79"
+    },
+    {
+        date: "June 20th, 2020",
+        description: "Golden Sun Bakery",
+        amount: "$50.00",
+        balance: "$2187.79"
+    }
+]
+
+export {transactionsDataHeader, transactionsData}
