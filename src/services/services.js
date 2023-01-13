4b768ea7597a7cee60ca05cd0dd848cbf3e07c46
ContactJHC <95849631+ContactJHC@users.noreply.github.com>
2023-01-13 14:26:05 +0100

    beginning of swagger file

--- src/services/services.js
+++ src/services/services.js
@@ -1,7 +1,106 @@
 // import { usersFetching, usersResolved, usersRejected } from "../features/users"
 
-// users data URL : 
-const loginURL = 'http://localhost:3001/api/v1/user/login'
+export async function postLogin(userName, password) {
+    try {
+        const result = await fetch('http://localhost:3001/api/v1/user/login', 
+            {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json'
+                },
+                // body: JSON.stringify({
+                //     'email': 'tony@stark.com',
+                //     'password': 'password123'
+                // })
+                body: JSON.stringify({
+                    'email': userName,
+                    'password': password
+                })
+            })
+            .then((res) => res.json())
+            .then((json) => json.body.token)
+            return result
+        } catch (err) {
+            console.log(err);
+            return null
+      }
+}
+
+export async function postSignup(email, password, firstName, lastName) {
+    try {
+        const result = await fetch('http://localhost:3001/api/v1/user/signup', 
+            {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json'
+                },
+                // body: JSON.stringify({
+                //     "email": "tony@stark.com",
+                //     "password": "password123",
+                //     "firstName": "tony",
+                //     "lastName": "pastark"
+                // })
+                body: JSON.stringify({
+                    "email": email,
+                    "password": password,
+                    "firstName": firstName,
+                    "lastName": lastName
+                })
+            })
+            .then((res) => res.json())
+            .then((json) => json.body.token)
+        return result
+    } catch (err) {
+        console.log(err);
+        return null
+    }
+}
+
+export async function postProfile(token) {
+    try {
+        const result = await fetch('http://localhost:3001/api/v1/user/profile', 
+            {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json',
+                'Authentication': `Bearer ${token}`
+                }
+            })
+            .then((res) => res.json())
+            .then((json) => json.body.token)
+        return result
+    } catch (err) {
+        console.log(err);
+        return null
+    }
+}
+
+export async function putProfile(token, firstName, lastName) {
+    try {
+        const result = await fetch('http://localhost:3001/api/v1/user/profile', 
+            {
+                method: 'PUT',
+                headers: {
+                'Content-Type': 'application/json',
+                'Authentication': `Bearer ${token}`
+                },
+                // body: JSON.stringify({
+                //     "firstName": "tony",
+                //     "lastName": "pastark"
+                // })
+                body: JSON.stringify({
+                    "firstName": firstName,
+                    "lastName": lastName
+                })
+            })
+            .then((res) => res.json())
+            .then((json) => json.body.token)
+        return result
+    } catch (err) {
+        console.log(err);
+        return null
+    }
+}
 
 // export default async function fetchOrUpdateUsers(store) {
 //     const status = store.getState().users.status
@@ -31,49 +130,3 @@ const loginURL = 'http://localhost:3001/api/v1/user/login'
 //     }
 // }
 
-export async function postLogin(userName, password) {
-    try {
-        const result = await fetch('http://localhost:3001/api/v1/user/login', 
-            {
-                method: 'POST',
-                headers: {
-                'Content-Type': 'application/json'
-                },
-                // body: JSON.stringify({
-                //     'email': 'tony@stark.com',
-                //     'password': 'password123'
-                // })
-                body: JSON.stringify({
-                    'email': userName,
-                    'password': password
-                })
-            })
-            .then((res) => res.json())
-            .then((json) => json.body.token)
-            return result
-        } catch (err) {
-            console.log(err);
-            return null
-      }
-
-    // let token = fetch('http://localhost:3001/api/v1/user/login', 
-    //     {
-    //         method: 'POST',
-    //         headers: {
-    //         'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify({
-    //             'email': 'tony@stark.com',
-    //             'password': 'password123'
-    //         })
-    //         // body: JSON.stringify({
-    //         //     'email': userName,
-    //         //     'password': password
-    //         // })
-    //     }
-    // )
-    // .then(resp => resp.json())
-    // .then(json => token = json.body.token)
-    // console.log(token);
-    // return token
-}
